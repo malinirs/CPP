a6b95f9ed6b52f46bb4735357d5adc3e72470eee
Kris <tyshchenko-k@inbox.ru>
2022-01-18 20:31:16 +0300

    Add files via upload

---
+++ ex00/Bureaucrat.cpp
@@ -0,0 +1,65 @@
+//
+// Created by Advisor Woods on 1/17/22.
+//
+
+#include "Bureaucrat.hpp"
+
+//оператор throw используется для сигнализирования о возникновении исключения или ошибки (аналогия тому, когда
+//свистит арбитр). Сигнализирование о том, что произошло исключение, называется генерацией исключения
+//(или «выбрасыванием исключения»).
+
+Bureaucrat::Bureaucrat(const std::string name_bureaucrat, int value) :
+name_bureaucrat(name_bureaucrat), value(value)
+{
+//	std::cout << "Constructor Bureaucrat called" << std::endl;
+	if (value > 150)
+		throw Bureaucrat::GradeTooLowException();
+	if (value < 1)
+		throw Bureaucrat::GradeTooHighException();
+
+}
+
+Bureaucrat::~Bureaucrat() {}
+//{ std::cout << "Destructor Bureaucrat called" << std::endl; }
+
+Bureaucrat::Bureaucrat(const Bureaucrat &cp)
+{ Bureaucrat::operator=(cp); }
+
+Bureaucrat	&Bureaucrat::operator=(const Bureaucrat &oper)
+{
+//	std::cout << "Bureaucrat assignation operator overload called" << std::endl;
+	if (this == &oper)
+		return (*this);
+	value = oper.value;
+	return (*this);
+}
+
+std::string	Bureaucrat::getName() const
+{ return (name_bureaucrat); }
+
+int	Bureaucrat::getGrade() const
+{ return (value); }
+
+const char*	Bureaucrat::GradeTooLowException::what() const throw()
+{ return ("The rating is below the minimum possible"); }
+
+const char*	Bureaucrat::GradeTooHighException::what() const throw()
+{ return ("The rating is higher the maximum possible"); }
+
+void	Bureaucrat::increase_value()
+{
+	if (--this->value < 1)
+		throw Bureaucrat::GradeTooHighException();
+}
+
+void	Bureaucrat::decrease_value()
+{
+	if (++this->value > 150)
+		throw Bureaucrat::GradeTooLowException();
+}
+
+std::ostream&	operator<<(std::ostream& out, Bureaucrat const& bureaucrat)
+{
+	out << "<" << bureaucrat.getName() << "> , bureaucrat grade <" << bureaucrat.getGrade() << ">";
+	return (out);
+}
---
+++ ex01/Bureaucrat.hpp
@@ -0,0 +1,50 @@
+//
+// Created by Advisor Woods on 1/17/22.
+//
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <string>
+# include "Form.hpp"
+
+class Form;
+
+class Bureaucrat
+{
+private:
+	std::string const	name_bureaucrat;
+	int					value;
+	Bureaucrat() {}; //не можем создать бюрократа без имени, потому этот конструктор в поле private
+
+public:
+	Bureaucrat(const std::string name_bureaucrat, int grade);
+	Bureaucrat(Bureaucrat const &cp);
+	Bureaucrat &operator=(Bureaucrat const &oper);
+	~Bureaucrat();
+
+	std::string	getName() const;
+	int			getGrade() const;
+
+	class GradeTooHighException : public std::exception
+	{
+		public:
+			virtual const char* what() const throw();
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+		public:
+			virtual const char* what() const throw();
+	};
+
+	void	decrease_value(); //увеличение
+	void	increase_value(); //уменьшение
+
+	void	signForm(Form &form);
+};
+
+std::ostream&	operator<<(std::ostream& out, Bureaucrat const& bureaucrat);
+
+#endif
